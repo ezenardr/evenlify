55d1e998d4859b9a6481353be245d62457450af0
ezenardr <ezenardr.dev@gmail.com>
2024-07-30 09:56:20 -0400

    Feat: admin dashboard page

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,7 +1,160 @@
-import React from "react";
+import Link from "next/link";
+import {
+  Activity,
+  ArrowUpRight,
+  CreditCard,
+  DollarSign,
+  Users,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-function AdminDashboard() {
-  return <div>Admin Dashboard</div>;
+export default function AdminDashboard() {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+          <Card x-chunk="dashboard-01-chunk-0">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Revenue Total
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">$0.00</div>
+              <p className="text-xs text-muted-foreground">
+                +0% from last month
+              </p>
+            </CardContent>
+          </Card>
+          <Card x-chunk="dashboard-01-chunk-1">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Abonnements</CardTitle>
+              <Users className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">+0</div>
+              <p className="text-xs text-muted-foreground">
+                +0% from last month
+              </p>
+            </CardContent>
+          </Card>
+          <Card x-chunk="dashboard-01-chunk-2">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Ventes</CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">+0</div>
+              <p className="text-xs text-muted-foreground">
+                +0% from last month
+              </p>
+            </CardContent>
+          </Card>
+          <Card x-chunk="dashboard-01-chunk-3">
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Evénements Actif
+              </CardTitle>
+              <Activity className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">+573</div>
+              <p className="text-xs text-muted-foreground">
+                +0 since last hour
+              </p>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="grid gap-4 md:gap-8 lg:grid-cols-2">
+          <Card className="" x-chunk="dashboard-01-chunk-4">
+            <CardHeader className="flex flex-row items-center">
+              <div className="grid gap-2">
+                <CardTitle>Transactions</CardTitle>
+                <CardDescription>Transactions Récente</CardDescription>
+              </div>
+              <Button asChild size="sm" className="ml-auto gap-1">
+                <Link href="#">
+                  View All
+                  <ArrowUpRight className="h-4 w-4" />
+                </Link>
+              </Button>
+            </CardHeader>
+            <CardContent>
+              <p className={"text-center"}>Empty</p>
+              {/*<Table>*/}
+              {/*  <TableHeader>*/}
+              {/*    <TableRow>*/}
+              {/*      <TableHead>Customer</TableHead>*/}
+              {/*      <TableHead className="hidden xl:table-column">*/}
+              {/*        Type*/}
+              {/*      </TableHead>*/}
+              {/*      <TableHead className="hidden xl:table-column">*/}
+              {/*        Status*/}
+              {/*      </TableHead>*/}
+              {/*      <TableHead className="hidden xl:table-column">*/}
+              {/*        Date*/}
+              {/*      </TableHead>*/}
+              {/*      <TableHead className="text-right">Amount</TableHead>*/}
+              {/*    </TableRow>*/}
+              {/*  </TableHeader>*/}
+              {/*  <TableBody>*/}
+              {/*    <TableRow>*/}
+              {/*      <TableCell>*/}
+              {/*        <div className="font-medium">Liam Johnson</div>*/}
+              {/*        <div className="hidden text-sm text-muted-foreground md:inline">*/}
+              {/*          liam@example.com*/}
+              {/*        </div>*/}
+              {/*      </TableCell>*/}
+              {/*      <TableCell className="hidden xl:table-column">*/}
+              {/*        Sale*/}
+              {/*      </TableCell>*/}
+              {/*      <TableCell className="hidden xl:table-column">*/}
+              {/*        <Badge className="text-xs" variant="outline">*/}
+              {/*          Approved*/}
+              {/*        </Badge>*/}
+              {/*      </TableCell>*/}
+              {/*      <TableCell className="hidden md:table-cell lg:hidden xl:table-column">*/}
+              {/*        2023-06-23*/}
+              {/*      </TableCell>*/}
+              {/*      <TableCell className="text-right">$250.00</TableCell>*/}
+              {/*    </TableRow>*/}
+              {/*  </TableBody>*/}
+              {/*</Table>*/}
+            </CardContent>
+          </Card>
+          <Card x-chunk="dashboard-01-chunk-5">
+            <CardHeader>
+              <CardTitle>Ventes Récentes</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-8">
+              <p className={"text-center"}>Empty</p>
+              {/*<div className="flex items-center gap-4">*/}
+              {/*  <Avatar className="hidden h-9 w-9 sm:flex">*/}
+              {/*    <AvatarImage src="/avatars/01.png" alt="Avatar" />*/}
+              {/*    <AvatarFallback>OM</AvatarFallback>*/}
+              {/*  </Avatar>*/}
+              {/*  <div className="grid gap-1">*/}
+              {/*    <p className="text-sm font-medium leading-none">*/}
+              {/*      Olivia Martin*/}
+              {/*    </p>*/}
+              {/*    <p className="text-sm text-muted-foreground">*/}
+              {/*      olivia.martin@email.com*/}
+              {/*    </p>*/}
+              {/*  </div>*/}
+              {/*  <div className="ml-auto font-medium">+$1,999.00</div>*/}
+              {/*</div>*/}
+            </CardContent>
+          </Card>
+        </div>
+      </main>
+    </div>
+  );
 }
-
-export default AdminDashboard;
--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,33 +1,23 @@
 "use client";
 import React from "react";
 import {
-  Bell,
   CircleUser,
   Home,
   Layers3,
   LineChart,
+  LogOut,
   Menu,
   Package2,
-  Search,
   ShoppingCart,
   Users,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import {
   Sheet,
   SheetClose,
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Input } from "@/components/ui/input";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -46,7 +36,7 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
       name: "Événements",
       path: "/admin/events",
       icon: <ShoppingCart className="h-4 w-4" />,
-      notifications: 6,
+      // notifications: 6,
     },
     {
       name: "Catégories",
@@ -79,10 +69,10 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
               <Package2 className="h-6 w-6" />
               <span className="">Evenlify</span>
             </Link>
-            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-              <Bell className="h-4 w-4" />
-              <span className="sr-only">Evenlify</span>
-            </Button>
+            {/*<Button variant="outline" size="icon" className="ml-auto h-8 w-8">*/}
+            {/*  <Bell className="h-4 w-4" />*/}
+            {/*  <span className="sr-only">Evenlify</span>*/}
+            {/*</Button>*/}
           </div>
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
@@ -93,41 +83,34 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
                 <Home className="h-4 w-4" />
                 Dashboard
               </Link>
-              {navigationLinks.map(
-                ({ name, path, icon, notifications }, key) => (
-                  <Link
-                    key={key}
-                    href={path}
-                    className={`flex items-center gap-3 rounded-lg px-3 py-2  transition-all hover:text-primary ${isPathActive(path) ? "bg-muted text-primary" : "text-muted-foreground"}`}
-                  >
-                    {icon}
-                    {name}
-                    {notifications && (
-                      <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                        {notifications}
-                      </Badge>
-                    )}
-                  </Link>
-                ),
-              )}
+              {navigationLinks.map(({ name, path, icon }, key) => (
+                <Link
+                  key={key}
+                  href={path}
+                  className={`flex items-center gap-3 rounded-lg px-3 py-2  transition-all hover:text-primary ${isPathActive(path) ? "bg-muted text-primary" : "text-muted-foreground"}`}
+                >
+                  {icon}
+                  {name}
+                </Link>
+              ))}
             </nav>
           </div>
-          <div className="mt-auto p-4">
-            <Card x-chunk="dashboard-02-chunk-0">
-              <CardHeader className="p-2 pt-0 md:p-4">
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                <Button size="sm" className="w-full">
-                  Upgrade
-                </Button>
-              </CardContent>
-            </Card>
-          </div>
+          {/*<div className="mt-auto p-4">*/}
+          {/*  <Card x-chunk="dashboard-02-chunk-0">*/}
+          {/*    <CardHeader className="p-2 pt-0 md:p-4">*/}
+          {/*      <CardTitle>Upgrade to Pro</CardTitle>*/}
+          {/*      <CardDescription>*/}
+          {/*        Unlock all features and get unlimited access to our support*/}
+          {/*        team.*/}
+          {/*      </CardDescription>*/}
+          {/*    </CardHeader>*/}
+          {/*    <CardContent className="p-2 pt-0 md:p-4 md:pt-0">*/}
+          {/*      <Button size="sm" className="w-full">*/}
+          {/*        Upgrade*/}
+          {/*      </Button>*/}
+          {/*    </CardContent>*/}
+          {/*  </Card>*/}
+          {/*</div>*/}
         </div>
       </div>
       <div className="flex flex-col">
@@ -162,54 +145,47 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
                     Dashboard
                   </Link>
                 </SheetClose>
-                {navigationLinks.map(
-                  ({ name, path, icon, notifications }, key) => (
-                    <SheetClose asChild key={key}>
-                      <Link
-                        href={path}
-                        className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 ${isPathActive(path) ? "bg-muted text-primary" : "text-muted-foreground"}  hover:text-foreground`}
-                      >
-                        {icon}
-                        {name}
-                        {notifications && (
-                          <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                            6
-                          </Badge>
-                        )}
-                      </Link>
-                    </SheetClose>
-                  ),
-                )}
+                {navigationLinks.map(({ name, path, icon }, key) => (
+                  <SheetClose asChild key={key}>
+                    <Link
+                      href={path}
+                      className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 ${isPathActive(path) ? "bg-muted text-primary" : "text-muted-foreground"}  hover:text-foreground`}
+                    >
+                      {icon}
+                      {name}
+                    </Link>
+                  </SheetClose>
+                ))}
               </nav>
-              <div className="mt-auto">
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Upgrade to Pro</CardTitle>
-                    <CardDescription>
-                      Unlock all features and get unlimited access to our
-                      support team.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <Button size="sm" className="w-full">
-                      Upgrade
-                    </Button>
-                  </CardContent>
-                </Card>
-              </div>
+              {/*<div className="mt-auto">*/}
+              {/*  <Card>*/}
+              {/*    <CardHeader>*/}
+              {/*      <CardTitle>Upgrade to Pro</CardTitle>*/}
+              {/*      <CardDescription>*/}
+              {/*        Unlock all features and get unlimited access to our*/}
+              {/*        support team.*/}
+              {/*      </CardDescription>*/}
+              {/*    </CardHeader>*/}
+              {/*    <CardContent>*/}
+              {/*      <Button size="sm" className="w-full">*/}
+              {/*        Upgrade*/}
+              {/*      </Button>*/}
+              {/*    </CardContent>*/}
+              {/*  </Card>*/}
+              {/*</div>*/}
             </SheetContent>
           </Sheet>
           <div className="w-full flex-1">
-            <form>
-              <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  type="search"
-                  placeholder="Search products..."
-                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                />
-              </div>
-            </form>
+            {/*<form>*/}
+            {/*  <div className="relative">*/}
+            {/*    <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />*/}
+            {/*    <Input*/}
+            {/*      type="search"*/}
+            {/*      placeholder="Search products..."*/}
+            {/*      className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"*/}
+            {/*    />*/}
+            {/*  </div>*/}
+            {/*</form>*/}
           </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -219,13 +195,17 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuLabel>Mon Compte</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Paramètres</DropdownMenuItem>
               <DropdownMenuItem>Support</DropdownMenuItem>
               <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => signOut()}>
-                Logout
+              <DropdownMenuItem
+                className={"flex gap-4 cursor-pointer"}
+                onClick={() => signOut()}
+              >
+                <LogOut color={"red"} />
+                Se Déconnecter
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
